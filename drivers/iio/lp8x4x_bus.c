f37eb00b90f3319bd9320db328f97d69a6f073d2
Sergey Yanovich <ynvich@gmail.com>
2013-03-22 22:24:11 +0400

    ARM: set LP-8x4x serial from built-in 1-wire

--- drivers/iio/lp8x4x_bus.c
+++ drivers/iio/lp8x4x_bus.c
@@ -10,10 +10,12 @@
  */
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/w1.h>
 #include <linux/w1-gpio.h>
 #include <linux/platform_device.h>
 
 #include <mach/mfp-pxa27x.h>
+#include <asm/system_info.h>
 
 #define MODULE_NAME	"lp8x4x_bus"
 MODULE_LICENSE("GPL");
@@ -21,6 +23,28 @@ MODULE_AUTHOR("Sergey Yanovich <ynvich@gmail.com>");
 MODULE_DESCRIPTION("ICP DAS LP-8x4x parallel bus driver");
 
 static void lp8x4x_noop_release(struct device *dev) {}
+
+static int lp8x4x_w1_notify(struct notifier_block *nb,
+	       	unsigned long state, void *dev)
+{
+	struct w1_slave *sl;
+	u8 sn[8];
+	u64 id;
+	if (state != W1_SLAVE_ADD)
+		return NOTIFY_DONE;
+
+	sl = (struct w1_slave *) dev;
+	sn[7] = sl->reg_num.family;
+	id = sl->reg_num.id;
+	memcpy(&sn[1], &id, 6);
+	sn[0] = sl->reg_num.crc;
+	id = *(u64 *) sn;
+	system_serial_high = (unsigned int) (id >> 32);
+	system_serial_low = (unsigned int) (id & 0xFFFFFFFF);
+	printk(KERN_INFO MODULE_NAME ": LP-8x4x serial %016llx\n", id);
+
+	return NOTIFY_DONE;
+}
 
 static struct w1_gpio_platform_data lp8x4x_w1_gpio_data = {
 	.pin			= MFP_PIN_GPIO83,
@@ -36,15 +60,21 @@ static struct platform_device lp8x4x_w1_master_device = {
 	}
 };      
 
+struct notifier_block lp8x4x_notifier = {
+	.notifier_call = lp8x4x_w1_notify,
+};
+
 static int __init lp8x4x_bus_init(void)
 {
 	platform_device_register(&lp8x4x_w1_master_device);
+	w1_register_notify(&lp8x4x_notifier);
 	printk(KERN_INFO MODULE_NAME ": loaded\n");
 	return 0;
 }
 
 static void __exit lp8x4x_bus_exit(void)
 {
+	w1_unregister_notify(&lp8x4x_notifier);
 	platform_device_unregister(&lp8x4x_w1_master_device);
 	printk(KERN_INFO MODULE_NAME ": unloaded\n");
 }
